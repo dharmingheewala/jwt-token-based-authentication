27017c5c8b755312618892888fc89c2ecc8fae79
dharmingheewala <d.gheewala1996@gmail.com>
2023-11-26 12:44:38 +0530

    initial commit

---
+++ jwt-token-based-authentication/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using jwt_token_based_authentication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace jwt_token_based_authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        private Users AutheticateUser(Users user)
+        {
+            Users _user = null;
+            if (user.UserName == "admin" && user.Password == "abcd1234")
+            {
+                _user = new Users { UserName = "Dharmin Gheewala" };
+            }
+            return _user;
+        }
+
+        private string GenerateToken(Users user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"], null, expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(Users user)
+        {
+            IActionResult response = Unauthorized();
+            var user_ = AutheticateUser(user);
+            if (user_ != null)
+            {
+                var token = GenerateToken(user_);
+                response = Ok(new { token = token });
+            }
+            return response;
+        }
+    }
+}
